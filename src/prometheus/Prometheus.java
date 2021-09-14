0b71893ad906bda0caa924ccdaaf08609cef7c21
Serhii <malgothsm@gmail.com>
2021-09-14 19:30:12 +0300

    Strings

--- src/prometheus/Prometheus.java
+++ src/prometheus/Prometheus.java
@@ -5,12 +5,16 @@ public class Prometheus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	      double recommendedWaterIntake = 8;
-	      double daysInChallenge = 30;
-	      double yourWaterIntake = 235.5;
-	      double totalRecommendedAmount = recommendedWaterIntake * daysInChallenge;
-	      boolean isChallengeComplete = yourWaterIntake >= totalRecommendedAmount;
-	      System.out.println(isChallengeComplete);
+		public class Song {
+		  	public static void main(String[] args){
+		      String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+		      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+		      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+		      System.out.println(line1.equals(line2));
+		      System.out.println(line2.equals(line3));
+		    }       
+		}
+
 	}
 
 }
