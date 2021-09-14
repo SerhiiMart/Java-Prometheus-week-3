6cdf2f3e43c2462d46685975a86b6a943d6452ac
Serhii <malgothsm@gmail.com>
2021-09-14 19:34:10 +0300

    zebras

--- src/prometheus/Prometheus.java
+++ src/prometheus/Prometheus.java
@@ -5,16 +5,10 @@ public class Prometheus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		public class Song {
-		  	public static void main(String[] args){
-		      String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
-		      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
-		      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
-		      System.out.println(line1.equals(line2));
-		      System.out.println(line2.equals(line3));
-		    }       
-		}
-
+		   int animals = 12;
+	        String species = "zebra";
+	        String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+	        System.out.println(zooDescription);
 	}
 
 }
