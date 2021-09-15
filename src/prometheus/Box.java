0ff62b1a6b5b694a0e5a569719dd6fb9579a56fc
Serhii <malgothsm@gmail.com>
2021-09-15 19:38:34 +0300

    Box project update

--- src/prometheus/Box.java
+++ src/prometheus/Box.java
@@ -1,6 +1,5 @@
 package prometheus;
 
-import java.awt.print.Printable;
 
 public class Box {
 	private int x;
@@ -19,5 +18,11 @@ public class Box {
 	
 	public void print() {
 		System.out.println("x: " + x + ", " + "y: " + y + ", " + "z: " + z);
+	}
+	
+	public void change(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 }
--- src/prometheus/BoxDemo.java
+++ src/prometheus/BoxDemo.java
@@ -7,6 +7,14 @@ public class BoxDemo {
 		Box b1 = new Box(10, 20, 40);
 		Box b2 = new Box(50, 10, 20);
 		Box b3 = new Box();
+		
+//		b1 = b2;
+//		b1.print();
+		
+//		b1= null;
+//		b1.print();
+		
+		b3.change(100, 56, 70);
 		b1.print();
 		b2.print();
 		b3.print();
