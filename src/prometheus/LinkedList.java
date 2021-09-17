2af8e89fd3253f2291ec8141ebc89e7b98b54897
Serhii <malgothsm@gmail.com>
2021-09-17 17:36:45 +0300

    List for Prometeus

---
+++ src/prometheus/LinkedList.java
@@ -0,0 +1,54 @@
+package prometheus;
+
+public class LinkedList {
+//    private int listIndex;
+//    private Node head;
+//    
+//    public LinkedList() {
+//        head = new Node();
+//        head.setData(null);
+//        listIndex = 0;
+//    }
+//
+//    public void add(Integer data) {
+//        Node newNode = new Node();
+//        Node currNodeNode = head;
+//        newNode.setData(data);
+//        while (currNodeNode.getNext() != null) {
+//            currNodeNode = currNodeNode.getNext();
+//        }
+//        currNodeNode.setNext(newNode);
+//        listIndex++;
+//    }
+//
+//    public Integer get(int index) {
+//        if (index < 0 || index >= size()) {
+//            return null;
+//        }
+//        Node currNode = head;
+//        for (int i = 0; i <= index; i++) {
+//            currNode = currNode.getNext();
+//        }
+//        return currNode.getData();
+//    }
+//
+//    public boolean delete(int index) {
+//        if (index < 0 || index > size()) {
+//            return false;
+//        }
+//        Node currNode = head;
+//        for (int i = 0; i < index; i++) {
+//            currNode = currNode.getNext();
+//        }
+//        currNode.setNext(currNode.getNext().getNext());
+//        listIndex--;
+//        return true;
+//    }
+//
+//    public int size() {
+//        return listIndex;
+//    }
+
+	
+	
+}   
